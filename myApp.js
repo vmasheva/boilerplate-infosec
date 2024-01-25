12ad9e3c8a95242862bd599a462cc817b2771fd8
vmasheva <vmasheva@gmail.com>
2024-01-25 15:01:57 +0000

    require helmet

--- myApp.js
+++ myApp.js
@@ -1,59 +1,14 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+const helmet = require("helmet");
 
 module.exports = app;
-const api = require('./server.js');
-app.use(express.static('public'));
-app.disable('strict-transport-security');
-app.use('/_api', api);
+const api = require("./server.js");
+app.use(express.static("public"));
+app.disable("strict-transport-security");
+app.use("/_api", api);
 app.get("/", function (request, response) {
-  response.sendFile(__dirname + '/views/index.html');
+  response.sendFile(__dirname + "/views/index.html");
 });
 let port = process.env.PORT || 3000;
 app.listen(port, () => {
