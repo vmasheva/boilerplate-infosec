5af21156b5c55637d8e39432d3506b81d49c3982
vmasheva <vmasheva@gmail.com>
2024-01-26 10:29:13 +0000

    noCache

--- myApp.js
+++ myApp.js
@@ -11,6 +11,7 @@ app.use(helmet.noSniff());
 app.use(helmet.ieNoOpen());
 app.use(helmet.hsts({ maxAge: ninetyDaysInSeconds, force: true }));
 app.use(helmet.dnsPrefetchControl());
+app.use(helmet.noCache());
 
 module.exports = app;
 const api = require("./server.js");
