4894650b2b4d206e6a1fe2fa1ff326fd95821a79
vmasheva <vmasheva@gmail.com>
2024-01-26 09:52:03 +0000

    noSniff

--- myApp.js
+++ myApp.js
@@ -5,6 +5,7 @@ const helmet = require("helmet");
 app.use(helmet.hidePoweredBy());
 app.use(helmet.frameguard({ action: "deny" }));
 app.use(helmet.xssFilter());
+app.use(helmet.noSniff());
 
 module.exports = app;
 const api = require("./server.js");
