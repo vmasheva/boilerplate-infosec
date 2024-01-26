71601b7ee07f9c19156a2ddc63256c01e1f51abc
vmasheva <vmasheva@gmail.com>
2024-01-26 10:36:35 +0000

    contentSecurityPolicy

--- myApp.js
+++ myApp.js
@@ -12,6 +12,14 @@ app.use(helmet.ieNoOpen());
 app.use(helmet.hsts({ maxAge: ninetyDaysInSeconds, force: true }));
 app.use(helmet.dnsPrefetchControl());
 app.use(helmet.noCache());
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: ["'self'"],
+      scriptSrc: ["'self'", "trusted-cdn.com"],
+    },
+  }),
+);
 
 module.exports = app;
 const api = require("./server.js");
