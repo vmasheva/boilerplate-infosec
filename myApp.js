a77cc2cffb959708ff8ae6ef03d0a2fa07520b6a
vmasheva <vmasheva@gmail.com>
2024-01-26 08:53:20 +0000

    use helmet

--- myApp.js
+++ myApp.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const helmet = require("helmet");
 
+app.use(helmet());
 helmet.hidePoweredBy();
 module.exports = app;
 const api = require("./server.js");
