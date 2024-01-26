f3c503c3b4c706e55fe0de9c0508fb1e4a6755dd
vmasheva <vmasheva@gmail.com>
2024-01-26 10:26:28 +0000

    dnsPrefetchControl

--- myApp.js
+++ myApp.js
@@ -10,6 +10,7 @@ app.use(helmet.xssFilter());
 app.use(helmet.noSniff());
 app.use(helmet.ieNoOpen());
 app.use(helmet.hsts({ maxAge: ninetyDaysInSeconds, force: true }));
+app.use(helmet.dnsPrefetchControl());
 
 module.exports = app;
 const api = require("./server.js");
